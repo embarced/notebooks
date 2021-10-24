ce03d7f62453d4de4047edc3443d5962bdfd0d34
Oliver Zeigermann <oliver@zeigermann.de>
2021-10-24 19:37:43 +0200

    new serving example

--- tfx/predict.sh
+++ tfx/predict.sh
@@ -1,2 +1,1 @@
-curl -d '{"instances": [[50.0, 122.0], [48, 100], [30, 150]]}' \
-    -X POST http://localhost:8501/v1/models/insurance:predict
+curl -d '{"instances": [[50.0, 122.0], [48, 100], [30, 150]]}' -X POST http://localhost:8501/v1/models/insurance:predict
